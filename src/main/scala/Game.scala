cea926571eba0c4f0c12d0b3ec091319a1b47714
vlbee <32312712+vlbee@users.noreply.github.com>
2018-12-03 00:00:46 +0000

    Add gameplay control flow for single player

--- src/main/scala/Game.scala
+++ src/main/scala/Game.scala
@@ -1,57 +1,47 @@
 package scrabble
-import scala.io.Source
 
 object Game extends App {
-  //Letters should be distributed based on the English Scrabble letter distribution (you may ignore blank tiles).
-  var letterBag: List[Tile] = Bag.initialise()
-  var player = Player(Nil, Nil)
+
+  // UTILITIES //
 
   // Assign seven tiles chosen randomly from the english alphabet to a rack.
-  // TODO test
-  // TODO need to use Option[Tile] & bag.headOption to handle empty Bag
-  // TODO how to handle letterBag var here? Scoping?
+  // TODO use option to handle empty bag
   def drawTile(bag: List[Tile]): Tile = {
-    val tile = bag.head // TODO use option
+    val tile = bag.head
     letterBag = bag.tail
     tile
   }
 
+  // Refill player rack after playing a word
+  def fillRack(rack: List[Tile]): List[Tile] = {
+    if (rack == Nil) List.fill(7)(drawTile(letterBag))
+    else rack ++ List.fill(7 - rack.length)(drawTile(letterBag))
+  }
+
   // TODO test
-  def draw(player: Player, number: Int): Player = {
-    // TODO player scoping?
-    // check if play has rack/Nil??
-    if (player.rack == Nil) {
-      // if no, draw 7 & return new Player with rack of 7
-      val newRack = List.fill(7)(drawTile(letterBag))
-      letterBag = letterBag.takeRight(letterBag.length - 7) // TODO redundant if drawTile() handles
-      Player(newRack, player.wordsPlayed)
-    } else {
-      // if yes draw difference to 7, return play with rack of 7
-      val refilledRack = player.rack ++ List.fill(number)(drawTile(letterBag))
-      letterBag = letterBag.takeRight(letterBag.length - number) // TODO redundant if drawTile() handles
-      Player(refilledRack, player.wordsPlayed)
-    }
+  def removeTilesFromRack(word: String, rack: List[Tile]): List[Tile] = {
+    val wordTiles: List[Tile] = word.toList.map(c => Tile(c))
+    rack diff wordTiles
   }
 
-  def playWord(player: Player): Player  = {
-    //Find a valid word formed from the seven tiles. A list of valid words can be found in twl06.txt.
-    val validDictionary: List[String] = Source.fromFile("src/main/data/validWords.txt").getLines.toList
-//    var word = 0;
-//    for (word <- 0 to 10) {
-//      println(validDictionary(word))
-//    }
-    player
+  def displayRack(player: Player): Unit = {
+    val rack = player.rack
+    print("Rack tiles:   ")
+    rack.map(tile => {
+      print(tile.letter + " ")
+    })
+    println()
+    print("Rack values:  ")
+    rack.map(tile => {
+      print(tile.value + " ")
+    })
+    println()
   }
 
-  def playerTurn(player: Player): Unit = {
-    // compose playWord and draw?
-    //plays word
-    //draws to refill rack based on word length
-  }
-
-  def gameStart(): Unit = {
-    player = Player(Nil, Nil) // reinitialize player with empty rack and word list
-    draw(player, 7) // draw new rack
+  def promptWord(player: Player): String = {
+    println("Play a word:")
+    val input = scala.io.StdIn.readLine()
+    input.toUpperCase
   }
 
   // Calculate the score for a word (you may ignore double/triple letter/word scores).
@@ -59,10 +49,70 @@ object Game extends App {
     word.toList.map(Tile(_).value).sum
   }
 
+  def isInDictionary(word: String): Boolean = {
+    if (Dictionary.all.contains(word.toLowerCase())) true else false
+  }
 
- playWord(player);
+  def isInRack(word: String, rack: List[Tile]): Boolean = {
+    val sortedWord = word.toList.sortWith(_.compareTo(_) < 0)
+    val sortedRack = rack.map(_.letter).sortWith(_.compareTo(_) < 0)
+
+    def loop(wL: List[Char], wI: Int, rL: List[Char], rI: Int ): Boolean  = {
+      if (wI + 1 == sortedWord.length) true
+      else if (rI + 1 == sortedRack.length) false
+      else if (wL(wI) == rL(rI)) loop(wL, wI + 1, rL, rI + 1)
+      else if (wL(wI) != rL(rI)) loop(wL, wI, rL, rI + 1)
+      else false
+    }
+
+    loop(sortedWord, 0, sortedRack, 0)
+  }
+
+  def isValidWord(word: String, rack: List[Tile]): Boolean = {
+    if (isInDictionary(word) && isInRack(word, rack)) true else false
+  }
+
+  // INITIALIZE GAME //
+
+  //Letters should be distributed based on the English Scrabble letter distribution (you may ignore blank tiles).
+  var letterBag: List[Tile] = Bag.initialise()
+  var player = Player(Nil, Nil, 0)
+  var computer = Player(Nil, Nil, 0)
 
 
+  // GAME PLAY //
+  def playerTurn(player: Player): (Player) => Player = {
+    displayRack(player);
+    val word: String = promptWord(player)
+    // forfeit turn and get a new rack
+    if (word == "") {
+      playerTurn(Player(fillRack(Nil), player.wordsPlayed, player.score))
+    } else if (!isValidWord(word, player.rack)) {
+      println(word + " is not a valid word. Try again.")
+      println("---------")
+      playerTurn(player)
+    } else {
+      val playedRack = removeTilesFromRack(word, player.rack)
+      val newScore = player.score + calculateWordScore(word)
+      println()
+      println("The word score for " + word + " is " + calculateWordScore(word))
+      println("Your total score is " + newScore)
+      println("---------")
+      playerTurn(Player(fillRack(playedRack), player.wordsPlayed ++ List(word), newScore))
+    }
+  }
+
+
+  def gameStart(): Unit = {
+    println()
+    println("Welcome to Scala Scrabble Practice")
+    println()
+    player = Player(fillRack(Nil), Nil, 0) // reinitialize player with empty rack and word list
+    println("This is your starting rack of letters:")
+    playerTurn(player)
+  }
+
+  gameStart();
 }
 
 
--- src/test/scala/GameSpec.scala
+++ src/test/scala/GameSpec.scala
@@ -1,27 +1,45 @@
 package scrabble
 
 import org.scalatest._
+import scrabble.Game.letterBag
 
 class GameSpec extends FlatSpec with Matchers {
 
-  "drawTile()" should "return a Tile" in {
+  "drawTile(bag)" should "return a Tile" in {
     var letterBag = Bag.initialise()
-    Game.drawTile(letterBag) shouldBe a [Tile]
+    Game.drawTile(letterBag) shouldBe a[Tile]
   }
 
-  "drawTile()" should "reduce letterBag length by 1" in {
+  "drawTile(bag)" should "reduce letterBag length by 1" in {
     var letterBag = Bag.initialise()
-    Game.drawTile(letterBag)
+    println(letterBag.length)
+    Game.drawTile(letterBag) // TODO this is changing the letterBag in the Game object??
+    println(letterBag.length)
     letterBag.length should equal(100)
   }
 
-  "calculateWordScore()" should "return correct word score" in {
+  "calculateWordScore(word)" should "return correct word score" in {
     Game.calculateWordScore("TEST") should equal(4);
     Game.calculateWordScore("QUICK") should equal(20);
   }
 
-  "draw(player, number)" should "return new rack of length 7" in {
-
+  "fillRack(player)" should "return a filled rack of length 7" in {
+    Game.fillRack(Nil) should have length 7
+    Game.fillRack(List(Tile('A'))) should have length 7
   }
+
+  "isInRack(word, rack)" should "return true if rack tiles can create word input" in {
+    Game.isInRack("CAT", List(Tile('T'), Tile('B'), Tile('G'), Tile('T'), Tile('A'), Tile('U'), Tile('C'))) shouldBe(true)
+    Game.isInRack("DOG", List(Tile('T'), Tile('B'), Tile('G'), Tile('T'), Tile('A'), Tile('U'), Tile('C'))) shouldBe(false)
+    Game.isInRack("TATUGBC", List(Tile('T'), Tile('B'), Tile('G'), Tile('T'), Tile('A'), Tile('U'), Tile('C'))) shouldBe(true)
+  }
+
+  "isInDictionary(word)" should "return true if word input is in Scrabble dictionary" in {
+    Game.isInDictionary("aardvark") shouldBe(true)
+    Game.isInDictionary("") shouldBe(false)
+    Game.isInDictionary("aa") shouldBe(true)
+    Game.isInDictionary("brejklg") shouldBe(false)
+  }
+
 
 }
