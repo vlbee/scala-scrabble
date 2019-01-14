d0d0b83c02217b754c1444be0155d41bd0e5150b
vlbee <32312712+vlbee@users.noreply.github.com>
2019-01-14 21:44:01 +0000

    Add Bag Class and spec

--- src/test/scala/BagSpec.scala
+++ src/test/scala/BagSpec.scala
@@ -1,3 +1,10 @@
-class BagSpec {
+import org.scalatest._
+
+class BagSpec extends FlatSpec with Matchers {
+
+  "initialize()" should "generate a random List of 101 Tiles" in {
+    Bag.initialise() should have length (101)
+    // TODO test for randomness
+  }
 
 }
