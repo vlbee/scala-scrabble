f3e4f92f6895fbf10986c245062b7c6f8463e08b
vanessa <32312712+vlbee@users.noreply.github.com>
2019-04-07 18:23:51 +0100

    Update Player/Human/Computer case classes

--- src/main/scala/model/Player.scala
+++ src/main/scala/model/Player.scala
@@ -1,9 +1,41 @@
 package model
 
-case class Player(name: String, rack: List[Tile], wordsPlayed: List[String], gameOver: Boolean)
+import scala.util.Random
 
-object Player {
-  def initialise(name: String): Player = {
-    Player(name, Nil, Nil, false)
+sealed trait Player {
+  def name: String
+
+  def rack: List[Tile]
+
+  def wordsPlayed: List[String]
+
+  def gameOver: Boolean
+
+  def calculateWordScore(word: String): Int = {
+    word.toList.map(Tile(_).value).sum
   }
+
+  def totalScore() = {
+    wordsPlayed.map(calculateWordScore(_)).sum
+  }
+
+}
+
+final case class Human(name: String, rack: List[Tile] = Nil, wordsPlayed: List[String] = Nil, gameOver: Boolean = false) extends Player
+
+final case class Computer(rack: List[Tile] = Nil, wordsPlayed: List[String] = Nil, gameOver: Boolean = false) extends Player {
+
+  val name: String = "The Computer"
+
+  def playWord(rack: List[Tile], level: Level): Option[String] = {
+    val possibleWords = Dictionary.filterPossibleWords(rack)
+
+    possibleWords.filter(_.nonEmpty).map { possibleWords =>
+      level match {
+        case Easy => Random.shuffle(possibleWords)
+        case Hard => possibleWords.sortBy(word => (word.length)).reverse
+      }
+    }.map(_.head.toUpperCase)
+  }
+
 }
