f986365cfa49048da9c76ea70002acd1268d9c96
vanessa <32312712+vlbee@users.noreply.github.com>
2019-04-07 18:24:13 +0100

    Update Dictionary with methods

--- src/main/scala/model/Dictionary.scala
+++ src/main/scala/model/Dictionary.scala
@@ -1,7 +1,33 @@
 package model
 
+import scala.annotation.tailrec
 import scala.io.Source
 
 object Dictionary {
+
   val all: List[String] = Source.fromFile("src/main/resources/dictionary.txt").getLines.toList
+
+  def isInDictionary(word: String): Boolean = {
+    Dictionary.all.contains(word.toLowerCase())
+  }
+
+  def isInRack(word: String, rack: List[Tile]): Boolean = {
+    @tailrec
+    def go(wordLetters: List[Char], rackLetters: List[Char]): Boolean = {
+      wordLetters match {
+        case Nil => true
+        case h :: t => rackLetters.contains(h) && go(t, rackLetters diff List(h))
+      }
+    }
+
+    go(word.toList, rack.map(_.letter))
+  }
+
+  def filterPossibleWords(rack: List[Tile]): Option[List[String]] = {
+    Dictionary.all.filter(word => isInRack(word.toUpperCase, rack)) match {
+      case Nil => None
+      case possibleWords => Some(possibleWords)
+    }
+  }
+
 }
